e66fe5622f3322afa68e4bb5e93fb1a6efac1313
LilianHoudelet <56303351+LilianHoudelet@users.noreply.github.com>
2021-05-03 16:42:43 +0200

    Delete webapp/target directory

--- webapp/target/classes/js/orderBy.js
+++
@@ -1,8 +0,0 @@
-
-(function ( $ ) {
-    $.fn.orderBy = function(attribut) {
-        $('#orderForm input[name=orderByAttribute]').attr('value',attribut);
-        $('#orderForm').submit();
-    };
-}( jQuery ));
-

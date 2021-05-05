a9d82e67de20e0af52984b49267029731f95701c
LilianHoudelet <lilian.houdelet@telecom-st-etienne.fr>
2021-05-05 10:39:28 +0200

    Add persistance.properties in HibernateConfig, Update datasource path

--- persistence/src/main/java/com/excilys/cdb/persistance/config/HibernateConfig.java
+++ persistence/src/main/java/com/excilys/cdb/persistance/config/HibernateConfig.java
@@ -1,5 +1,7 @@
 package com.excilys.cdb.persistance.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,7 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.excilys.cdb.logger.LoggerCdb;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -16,10 +19,13 @@ import com.zaxxer.hikari.HikariDataSource;
 @EnableTransactionManagement
 @ComponentScan({ "com.excilys.cdb.dao", "com.excilys.cdb.config" })
 public class HibernateConfig {
+	
+	private static final String PROP_DB = "/config/datasource.properties";
+	private static final String PROP_PERSISTENCE = "/config/persistence.properties";
 
 	@Bean("dataSource")
 	public HikariDataSource getDataSource() {
-		return new HikariDataSource(new HikariConfig("/com/excilys/cdb/dao/datasource.properties"));
+		return new HikariDataSource(new HikariConfig(PROP_DB));
 	}
 
 	@Bean
@@ -27,8 +33,7 @@ public class HibernateConfig {
 		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
 		factoryBean.setDataSource(getDataSource());
 		factoryBean.setPackagesToScan("com.excilys.cdb.dto");
-		factoryBean.setHibernateProperties(
-				(Properties) new Properties().setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect"));
+		factoryBean.setHibernateProperties(hibernateProperties());
 		return factoryBean;
 	}
 
@@ -38,5 +43,14 @@ public class HibernateConfig {
 		transactionManager.setSessionFactory(getSessionFactory().getObject());
 		return transactionManager;
 	}
-
+	
+	private final Properties hibernateProperties() {
+		Properties hibernateProperties = new Properties();
+		try (InputStream in = this.getClass().getResourceAsStream(PROP_PERSISTENCE)) {
+			hibernateProperties.load(in);
+		} catch (IOException e) {
+			LoggerCdb.logError(this.getClass(), e);
+		}
+		return hibernateProperties;
+	}
 }
